429bb502c012c5f6efc13ba5f8fe584545ddac5c
Wes Harrison <wesharrison89@gmail.com>
2023-05-16 19:15:49 -0700

    missed save files from last push

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <p>
+        Hello! My name is Wesley Harrison and welcome to my portfolio! 
+      </p>
+    </div>
+  );
+}
