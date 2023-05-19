03cd21311980d38754d47eecb4c528427410db0e
Wes Harrison <wesharrison89@gmail.com>
2023-05-18 19:35:43 -0700

    added form to contact

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,18 +1,79 @@
 import React from 'react';
 
 export default function Contact() {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [comment, setComment] = useState('');
+
+    const handleInputChange = (e) => {
+        
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+    
+        
+        if (inputType === 'name') {
+          setName(inputValue);
+        } else if (inputType === 'email') {
+          setEmail(inputValue);
+        } else if (inputType === 'comment') {
+          setComment(inputValue);
+        }
+      };
+  
+      const handleFormSubmit = (e) => {
+        // Preventing the default behavior of the form submit (which is to refresh the page)
+        e.preventDefault();
+    
+        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
+        if (!validateEmail(email) || !name) {
+          setErrorMessage('Email or username is invalid');
+          // We want to exit out of this code block if something is wrong so that the user can correct it
+          return;
+          // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
+        }
+        if (!checkPassword(password)) {
+          setErrorMessage(
+            `Choose a more secure password for the account: ${userName}`
+          );
+          return;
+        }
+        alert(`Hello ${userName}`);
+    
+        // If everything goes according to plan, we want to clear out the input after a successful registration.
+        setUserName('');
+        setPassword('');
+        setEmail('');
+      };
+
+
   return (
     <div>
       <h1>Contact Me</h1>
-      <p>
-      Email
-      </p>
-      <p>
-        Github
-      </p>
-      <p>
-        LinkedIn
-      </p>
+      <form className="form mb-3">
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="name"
+          placeholder="name"
+        />
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="email"
+        />
+        <input
+          value={comment}
+          name="comment"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Comment"
+        />
+        <button type="button" onClick={handleFormSubmit}>Submit</button>
+      </form>
     </div>
   );
 }
