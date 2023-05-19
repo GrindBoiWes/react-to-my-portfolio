9093da61daef414b0866bb99bfb873cdd521d2e6
Wes Harrison <wesharrison89@gmail.com>
2023-05-18 19:59:24 -0700

    debugging contact form

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { useState } from "react";
+import { validateEmail } from '../../utils/helpers';
+
 
 export default function Contact() {
     const [email, setEmail] = useState('');
@@ -22,20 +25,16 @@ export default function Contact() {
       };
   
       const handleFormSubmit = (e) => {
-        // Preventing the default behavior of the form submit (which is to refresh the page)
+        
         e.preventDefault();
-    
-        // First we check to see if the email is not valid or if the userName is empty. If so we set an error message to be displayed on the page.
         if (!validateEmail(email) || !name) {
-          setErrorMessage('Email or username is invalid');
-          // We want to exit out of this code block if something is wrong so that the user can correct it
           return;
-          // Then we check to see if the password is not valid. If so, we set an error message regarding the password.
         }
+        
         
         alert(`Hello ${name}`);
     
-        // If everything goes according to plan, we want to clear out the input after a successful registration.
+      
         setName('');
         setEmail('');
         setComment('');
